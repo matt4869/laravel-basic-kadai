627f131802776cd8e595571f64e85c3b3ff3b3d8
古山裕太 <koyamayuuta@furuyamayuutanoMacBook-Air.local>
2023-11-05 21:27:28 +0900

    first commit

--- laravel-basic/app/Http/Controllers/PostController.php
+++ laravel-basic/app/Http/Controllers/PostController.php
@@ -17,11 +17,5 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));   
     } 
 
-    public function show($id) {
-        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
-        $post = Post::find($id);
-
-        // 変数$productをproducts/show.blade.phpファイルに渡す
-        return view('posts.show', compact('post'));
-    }
+    
 }
--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -17,6 +17,5 @@ use App\Http\Controllers\PostController;
 
 
 
-Route::get('/post', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/{id}', [PostController::class, 'show']);
